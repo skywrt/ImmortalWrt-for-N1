5a70a0478d107513668557ddee7f806db25eac4b
skywrt <156793219+skywrt@users.noreply.github.com>
2024-07-25 22:18:19 +0800

    Update diy.sh

--- N1/diy.sh
+++ N1/diy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Default IP
-sed -i 's/192.168.1.1/192.168.2.2/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.4/g' package/base-files/files/bin/config_generate
 
 # Remove packages
 rm -rf feeds/luci/applications/luci-app-passwall
